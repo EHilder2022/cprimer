60f73b14d05204859c335877571529c81361dcc7
Aaecus <yao1364388625@gmail.com>
2022-11-23 10:29:38 +0800

    整型值在不同进制下的表示

---
+++ chapter3/bases.c
@@ -0,0 +1,16 @@
+//@Author : yaoyong
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+// 整形值在不同进制下的表现
+int main(int argc, char* argv[]){
+	int x = 100;
+	printf("dec = %d; octal = %o; hex = %x\n", x, x, x);
+	printf("dec = %d; octal = %#o; hex = %#x\n", x, x, x);
+	return 0;
+}
+
