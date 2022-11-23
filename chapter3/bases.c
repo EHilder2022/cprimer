1f4e61bf124e2c5f964dd91b8d5b73920d386d14
EHilder2022 <112707598+EHilder2022@users.noreply.github.com>
2022-11-23 10:47:15 +0800

    Update bases.c
    更改了一个字符错误

--- chapter3/bases.c
+++ chapter3/bases.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
-// 整形值在不同进制下的表现
+// 整型值在不同进制下的表现
 int main(int argc, char* argv[]){
 	int x = 100;
 	printf("dec = %d; octal = %o; hex = %x\n", x, x, x);
